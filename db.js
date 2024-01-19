d9071c1a2f119a800fb9487a583979e8056478aa
FGuerriero <fernandoguerrierocs@gmail.com>
2024-01-19 10:42:26 -0300

    added dialectModule setting on db.js

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+import mysql2 from 'mysql2';
 
 const { Sequelize } = require('sequelize')
 const User = require('./models/User')
@@ -5,8 +6,9 @@ const DB_CONFIG = require('./dbconfig')
 
 const connection = new Sequelize(DB_CONFIG)
 User.init(connection)
-if (DB_CONFIG.dialect == 'mysql') {
+if (DB_CONFIG.dialect === 'mysql') {
     connection.query('SET time_zone = "-03:00" ')
+    DB_CONFIG.dialectModule = mysql2;
 }
 module.exports = connection
 
